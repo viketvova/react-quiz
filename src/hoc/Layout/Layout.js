b3aef55b800467b30930a688188c5178fd037bb1
Vitaly <viketvova@gmail.com>
2021-02-11 16:06:44 +0200

    Add Navigation component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import classes from './Layout.module.css'
+import Menu from '../../components/Navigation/Menu/Menu'
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
     render() {
         return (
             <div className={classes.Layout}>
+
+                <Menu
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
+
                 <main>
                     {this.props.children}
                 </main>
